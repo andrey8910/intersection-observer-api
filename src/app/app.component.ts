93519a3dcff54aaaf861de16405208379a609382
ivanov6444 <a.ivanov6444@gmail.com>
2023-01-30 17:25:00 +0200

    build by hub pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from "@angular/router";
+import {Meta, Title} from "@angular/platform-browser";
+import {filter, tap} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'intersection-observer-api';
+
+  private ILS = 'img-loading-scroll';
+  private VPOS = 'video-play-on-scroll';
+  private MAOS = 'menu-active-on-scroll';
+  private IS = 'infinite-scroll';
+
+
+
+  private PAGE_LIST = {
+    [this.ILS] : "Завантаження картинки при скролі",
+    [this.VPOS] : "Зупинка програвання відео при скролі",
+    [this.MAOS] : "Зміна активного пункту меню при скролі",
+    [this.IS] : "Нескінчкнній скрол",
+
+  }
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title,
+    private metaService: Meta
+  ) {
+  }
+
+
+  ngOnInit() {
+    this.init();
+  }
+
+  private init(){
+    this.router.events.pipe(
+      filter((event:any) => event instanceof NavigationEnd),
+      tap((res) => {
+        const v = res.url.slice(1);
+        this.titleService.setTitle(this.PAGE_LIST[v] ? this.PAGE_LIST[v] : 'Scroll');
+        console.log(this.metaService.getTag("property='og:site_name'")?.content)
+      })
+    ).subscribe()
+
+
+  }
+
+
 }
